9e73aff179fb45f9e164dade0fa62c684b22bc0e
Jason Knight <binarybana@gmail.com>
2022-03-13 21:04:31 -0700

    Remove Heim as a dependency (it looks to be abandoned)

--- src/targets/mod.rs
+++ src/targets/mod.rs
@@ -3,11 +3,10 @@ mod target;
 pub use target::Target;
 
 pub fn local_target() -> Target {
-    let platform = futures::executor::block_on(heim::host::platform()).unwrap();
-    match platform.system() {
-        "Darwin" => {
-            let cmake_defines = match platform.architecture() {
-                heim::host::Arch::Unknown | heim::host::Arch::AARCH64 => {
+    match env!("TARGET_OS") {
+        "macos" => {
+            let cmake_defines = match env!("TARGET_ARCH") {
+                "aarch64" => {
                     vec![("CMAKE_OSX_ARCHITECTURES".into(), "arm64".into())]
                 }
                 _ => vec![],
@@ -19,23 +18,17 @@ pub fn local_target() -> Target {
                 cmake_defines,
             }
         }
-        "Linux" => {
-            let target = match platform.architecture() {
-                heim::host::Arch::AARCH64 => "aarch64-unknown-linux-gnu",
-                heim::host::Arch::X86_64 => "x86_64-unknown-linux-gnu",
-                _ => panic!("not supported"),
-            };
-
+        "linux" => {
             Target {
                 host: "Linux".into(),
-                target_str: target.into(),
+                target_str: env!("TARGET").into(),
                 cmake_defines: vec![],
             }
         }
         _ => {
             panic!(
                 "Platform `{}` unsupported, please check the issue tracker.",
-                platform.system()
+                env!("TARGET_OS")
             );
         }
     }
